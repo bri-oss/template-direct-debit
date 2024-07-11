4a1098966bd2ddc69adce1f14d5501ce417d6b18
AlfaRiza <malfariza45@gmail.com>
2024-07-11 16:44:45 +0700

    feat: templates refund

---
+++ src/refundNotify.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
+
+require __DIR__ . '/../../briapi-sdk/autoload.php';
+
+use BRI\Util\GetAccessToken;
+use BRI\DirectDebit\DirectDebit;
+
+$clientId = $_ENV['CONSUMER_KEY']; // customer key
+$clientSecret = $_ENV['CONSUMER_SECRET']; // customer secret
+$pKeyId = $_ENV['PRIVATE_KEY']; // private key
+
+// url path values
+$baseUrl = 'https://sandbox.partner.api.bri.co.id'; //base url
+
+// change variables accordingly
+$partnerId = 'feedloop'; //partner id
+$channelId = '12345'; // channel id
+
+$getAccessToken = new GetAccessToken();
+
+[$accessToken, $timestamp] = $getAccessToken->get(
+  $clientId,
+  $pKeyId,
+  $baseUrl
+);
+
+$directDebit = new DirectDebit();
+
+$response = $directDebit->refundNotify(
+  $clientSecret = $clientSecret, 
+  $partnerId = $partnerId,
+  $baseUrl,
+  $accessToken,
+  $channelId,
+  $timestamp
+);
+
+echo $response;
