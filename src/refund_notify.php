2b4101ecf2ee9ede03f04989ce5ef66ffd08321d
AlfaRiza <malfariza45@gmail.com>
2024-10-09 13:19:08 +0700

    update: add variable

--- src/refund_notify.php
+++ src/refund_notify.php
@@ -5,8 +5,8 @@ use BRI\Util\GetAccessToken;
 
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
-$clientId = '';
-$clientSecret = '';
+$clientId = 'YOWoKgXf5KcATtetyq7NbfxOz6FR65Un';
+$clientSecret = 'super_secret';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
